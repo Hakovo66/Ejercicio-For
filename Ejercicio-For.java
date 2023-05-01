f8e890c1dac8cbea3ef495a6da99d3845fde9e91
Hakovo66 <113375465+Hakovo66@users.noreply.github.com>
2023-04-30 23:11:14 -0600

    Create Ejercicio-For.java

---
+++ Ejercicio-For.java
@@ -0,0 +1,15 @@
+public class EjercicioFOR {
+
+    public static void main(String[] args) {
+    
+    int numeroFOR = 0;
+
+    for (numeroFOR = 0; numeroFOR <= 3; numeroFOR ++) {
+
+        System.out.println(numeroFOR);
+
+    }
+ }
+
+    
+}
